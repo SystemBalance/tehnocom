1bbfaf76cd0a18807e78393c911d921d0e731c57
SystemBalance <a.maisuradze@gmail.com>
2014-06-17 15:28:14 +0400

    Scaling god)

--- js/scaling.js
+++ js/scaling.js
@@ -1,21 +1,15 @@
+var readyHeight = 0;
+
 $( document ).ready(function() {
 	//пересчитываем зум
 	function changeZoom() {
+		freeHeight = $('.out').height();
 		freeWidth = document.body.clientWidth;
-		freeHeight = document.body.clientHeight;
-        
-//		freeWidth = document.body.clientWidth;
-//		freeHeight = document.body.clientHeight;
-
-//        offsetHeight
-
-//        wHeight = document.hei
-
 		tmpzoom = freeWidth/2560;
 		setZoom(tmpzoom);
 		$('.out').css('marginLeft', ((freeWidth-2560)/2) + 'px');
-		$('.out').css('marginTop', ((freeHeight - freeHeight/tmpzoom)/2) + 'px');
-		//$('body').css('height', (freeHeight - ((freeHeight - freeHeight/tmpzoom)/2)) + 'px');
+		$('.out').css('marginTop', ((freeHeight*tmpzoom) - freeHeight)/2 + 'px');
+		$('body').css('height', freeHeight + ((freeHeight*tmpzoom) - freeHeight)/2 + 'px');
 	}
 	
 	//пересчитываем зум
@@ -27,7 +21,7 @@ $( document ).ready(function() {
 		$('.out').css({'transform':'scale(' + zoom + ')'});
 	}
 	
-	//changeZoom();
+	changeZoom();
 	
 	window.onresize = function(e){
 		changeZoom();
