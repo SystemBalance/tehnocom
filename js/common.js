bfb268092e25d3759e80c891d1ae31b34c7f26a6
SystemBalance <a.maisuradze@gmail.com>
2014-06-20 17:07:59 +0400

    del init2

--- js/common.js
+++ js/common.js
@@ -28,14 +28,14 @@ head.ready(function() {
 	slider();
 	
 	//window scroll
-	$(window).scroll(function(){
-		var offset_top = $(document).scrollTop(),
-				map = $('.map'),
-				map_top = map.offset().top;
-		if (offset_top > map_top) {
-			map.addClass('is-animated');
-		};
-	});
+//	$(window).scroll(function(){
+//		var offset_top = $(document).scrollTop(),
+//				map = $('.map'),
+//				map_top = map.offset().top;
+//		if (offset_top > map_top) {
+//			map.addClass('is-animated');
+//		};
+//	});
 
 
 
--- js/init2.js
+++
@@ -1,1 +0,0 @@
-head.load("js/lib/jquery.js", "js/lib/cycle2.js", "js/lib/cycle2.carousel.js", "js/lib/jquery.mousewheel.js", "js/common2.js");
