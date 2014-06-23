a2211a3aa1f45506236616ca3255b18b879e76ed
SystemBalance <a.maisuradze@gmail.com>
2014-06-23 05:45:35 +0400

    Reg fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-    $(".domtabs li a").click(function(e) {
+    //BEGIN script табы на странице cat_2.html
+	$(".domtabs li a").click(function(e) {
     e.preventDefault();
     $(".domtabs li a").removeClass('d_activetab');
     $(this).addClass('d_activetab');
-  })
+  });
+	//END script табы на странице cat_2.html
+  	
+	//BEGIN script табы на странице registration.html
+	$('.d_pushone').click(function(){
+		$(this).addClass('d_green');
+		$('.d_box1').show();
+		$('.d_box2').hide();
+		$('.d_pushtwo').removeClass('d_blue');
+    });
+	$('.d_pushtwo').click(function(){
+		$(this).addClass('d_blue');
+		$('.d_box2').show();
+		$('.d_box1').hide();
+		$('.d_pushone').removeClass('d_green');
+    });
+	//END script табы на странице registration.html
 });
+
