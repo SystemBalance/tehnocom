1d83190c5dfe168f2af28b649da64ad12f485179
tigresse <alena624@mail.ru>
2014-07-31 19:01:31 +0400

    registration form 2nd

--- init.php
+++ init.php
@@ -5,7 +5,7 @@ class MyClass
    function OnAfterUserRegisterHandler(&$arFields)
 	{
 	     $message = "Информационное сообщение сайта Интернет-магазин\n";
-	     $message.="Ваш логин: ".$arFields["LOGIN"]."\nВаш парол: ".$arFields["PASSWORD"]."\n";
+	     $message.="Ваш логин: ".$arFields["LOGIN"]."\nВаш пароль: ".$arFields["PASSWORD"]."\n";
 	     mail($arFields["EMAIL"], 'Регистрационные данные с сайта shop.ru', $message, 'sale@shop.ru');
 	}
 }
